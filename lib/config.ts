0fe4d4f76c5e177d4ab2f6f9f6aa88c1fbc06774
Willie Xu <ity@keemail.me>
2022-04-23 22:15:06 +0800

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
-export const domain = `wll.moe`;
+export const domain = `Your Domain`;
 export const baseUrl = `https://${domain}`;
-export const copyright = `Copyright ${new Date().getFullYear()} - Willie`;
+export const copyright = `Copyright ${new Date().getFullYear()} - YourName`;
